7de5db744c209648839a9150f7beb4b5269209c1
dwest06 <dave00dark@gmail.com>
2017-11-19 09:28:31 -0400

    slider

--- app/controllers/perfil_controller.rb
+++ app/controllers/perfil_controller.rb
@@ -1,12 +1,18 @@
 class PerfilController < ApplicationController
 	def inicio
 		if usuario_signed_in?
-			@nombre = current_usuario.email
+			@nombre = current_usuario.usuario
 		end
 
 	end
 
 	def inversor
+		lista_proyectos = Proyecto.all
+		for i in 1..lista_proyectos.length
+			if current_usuario.id == lista_proyectos.id
+				@inversor = lista_proyectos.
+			end
+		end
 		@inversor = 
 		@cantidad_inversor
 	end
