041a49226d19f36bc4e972a0aaeb49a16eceda18
Andrés Ignacio Torres <andresitorresm@gmail.com>
2017-11-19 12:46:33 -0400

    Avance en funcionalidad de campaña

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -2,5 +2,5 @@ class Proyecto < ApplicationRecord
   validates :nombre, presence: true, length: { minimum: 5 }
   validates :sinopsis, presence: true, length: { minimum: 5 }
   validates :descripcion, presence: true, length: { minimum: 100 } 
-  validates :autor, :solicita_talentos, :solicita_dinero, presence: true
+  validates :autor, presence: true
 end
